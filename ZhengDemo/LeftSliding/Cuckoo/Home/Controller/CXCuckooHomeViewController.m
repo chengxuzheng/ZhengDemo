4b9fd20e8b3414fd77fe34c2b2d11a87210ad147
chengxuzheng <zheng890715@gmail.com>
2017-04-24 11:51:40 +0800

    加入定时器,背景音乐

--- ZhengDemo/LeftSliding/Cuckoo/Home/Controller/CXCuckooHomeViewController.m
+++ ZhengDemo/LeftSliding/Cuckoo/Home/Controller/CXCuckooHomeViewController.m
@@ -12,11 +12,20 @@
 
 @interface CXCuckooHomeViewController ()
 
+@property (nonatomic, strong) UIImageView *bgImgView; //背景图
 @property (nonatomic, strong) CXTimeActionView *bTimeView; //底部时间视图
+@property (nonatomic, strong) UIButton *voiceBtn; //音量
+@property (nonatomic, strong) UIButton *guideBtn;
+@property (nonatomic, strong) UIButton *recordBtn;
 
 @end
 
 @implementation CXCuckooHomeViewController
+
+#pragma mark - Button Action
+- (void)voiceBtnAction:(UIButton *)sender {
+    sender.selected = !sender.selected;
+}
 
 #pragma mark - View LifeCycle
 - (void)viewDidLoad {
@@ -34,9 +43,28 @@
     [self addSuviewsInView];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    _recordBtn.hidden = NO;
+    _guideBtn.hidden = NO;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    _recordBtn.hidden = YES;
+    _guideBtn.hidden = YES;
+}
+
 #pragma mark - Subviews
 - (void)addSuviewsInView {
     [self.view addSubview:self.bTimeView];
+    [self.view addSubview:self.bgImgView];
+    [self.view addSubview:self.voiceBtn];
+    
+    [self.navigationController.view addSubview:self.recordBtn];
+    [self.navigationController.view addSubview:self.guideBtn];
     
     [self layoutSubviewsInView];
 }
@@ -46,6 +74,61 @@
         make.left.right.bottom.equalTo(self.view);
         make.height.mas_equalTo(kCX_Scale(190));
     }];
+    
+    [_bgImgView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.top.right.equalTo(self.view);
+        make.bottom.equalTo(_bTimeView.mas_top);
+    }];
+    
+    [_voiceBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(kCX_Scale(20));
+        make.width.height.mas_equalTo(kCX_Scale(34));
+        make.right.mas_equalTo(-kCX_Scale(25));
+    }];
+    
+    [_recordBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.mas_equalTo(-kCX_Scale(20));
+        make.top.mas_equalTo(kCX_Scale(26));
+        make.width.height.mas_equalTo(kCX_Scale(32));
+    }];
+    
+    [_guideBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(_recordBtn.mas_left).offset(-kCX_Scale(20));
+        make.top.width.height.equalTo(_recordBtn);
+    }];
+}
+
+- (UIButton *)guideBtn {
+    if (!_guideBtn) {
+        _guideBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_guideBtn setImage:kIMAGE(@"tip") forState:UIControlStateNormal];
+    }
+    return _guideBtn;
+}
+
+- (UIButton *)recordBtn {
+    if (!_recordBtn) {
+        _recordBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_recordBtn setImage:kIMAGE(@"archive_mini") forState:UIControlStateNormal];
+    }
+    return _recordBtn;
+}
+
+- (UIButton *)voiceBtn {
+    if (!_voiceBtn) {
+        _voiceBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_voiceBtn setImage:kIMAGE(@"soundEnabled") forState:UIControlStateNormal];
+        [_voiceBtn setImage:kIMAGE(@"soundMute") forState:UIControlStateSelected];
+        [_voiceBtn addTarget:self action:@selector(voiceBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _voiceBtn;
+}
+
+- (UIImageView *)bgImgView {
+    if (!_bgImgView) {
+        _bgImgView = [[UIImageView alloc] initWithImage:kIMAGE(@"timg")];
+    }
+    return _bgImgView;
 }
 
 - (CXTimeActionView *)bTimeView {
--- ZhengDemo/LeftSliding/Cuckoo/Home/View/CXTimeActionView.m
+++ ZhengDemo/LeftSliding/Cuckoo/Home/View/CXTimeActionView.m
@@ -8,17 +8,26 @@
 
 #import "CXTimeActionView.h"
 
+#import <AVFoundation/AVFoundation.h>
+
+static const NSTimeInterval kMinTime = 10;
+static const NSTimeInterval kMaxTime = 55;
+static const NSTimeInterval kTimeInterval = 5;
+static const NSTimeInterval kInitialTime = 25;
+
 @interface CXTimeActionView ()
 
 @property (nonatomic, strong) UILabel *leftInfoLbl;
 @property (nonatomic, strong) UILabel *timeTitleLbl;
 @property (nonatomic, strong) UILabel *timeShowLbl;
-@property (nonatomic, strong) UIButton *subBtn;
-@property (nonatomic, strong) UIButton *addBtn;
 @property (nonatomic, strong) UISegmentedControl *segment;
 @property (nonatomic, strong) UIButton *timeActionBtn;
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, strong) AVAudioPlayer *player;
 
 @property (nonatomic, assign) NSInteger tomatoTime; //番茄时间
+@property (nonatomic, assign) NSInteger tomatoSeconds; //计时秒数
+@property (nonatomic, assign) BOOL isFire; //定时器是否开启
 
 @end
 
@@ -29,25 +38,81 @@
 - (void)tomatoTimeIsChanged:(UISegmentedControl *)sender {
     
     if (sender.selectedSegmentIndex == 0) {
-        if (_tomatoTime == 5) {
+        if (_tomatoTime == kMinTime) {
             [_segment setEnabled:NO forSegmentAtIndex:0];
         } else {
-            if (_tomatoTime == 60) {
+            if (_tomatoTime == kMaxTime+5) {
                 [_segment setEnabled:YES forSegmentAtIndex:1];
             }
-            _tomatoTime -= 5;
         }
+        _tomatoTime -= kTimeInterval;
+        _tomatoSeconds -= 300;
     } else {
-        if (_tomatoTime == 60) {
+        if (_tomatoTime == kMaxTime) {
             [_segment setEnabled:NO forSegmentAtIndex:1];
         } else {
-            if (_tomatoTime == 5) {
+            if (_tomatoTime == kMinTime-5) {
                 [_segment setEnabled:YES forSegmentAtIndex:0];
             }
-            _tomatoTime += 5;
+        }
+        _tomatoTime += kTimeInterval;
+        _tomatoSeconds += 300;
+    }
+    
+    if (_isFire) {
+        _timeShowLbl.text = [self getTimeSecondsStr];
+    } else {
+        _timeShowLbl.text = [NSString stringWithFormat:@"%ld分钟",_tomatoTime];
+    }
+    
+    [UIView animateWithDuration:kANIMATION_DURATION/2 animations:^{
+        _timeShowLbl.transform = CGAffineTransformMakeScale(1.3, 1.3);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:kANIMATION_DURATION/2 animations:^{
+            _timeShowLbl.transform = CGAffineTransformMakeScale(1, 1);
+        }];
+    }];
+}
+
+#pragma mark 开始计时
+- (void)timeActionBtnAction:(UIButton *)sender {
+    _tomatoSeconds = _tomatoTime * 60;
+    _isFire = YES;
+    [self.player play];
+    [self.timer fire];
+}
+
+#pragma mark 定时器
+- (void)timerFired:(NSTimer *)t {
+    if (_tomatoSeconds == 0) {
+        [t invalidate];
+        _timer = nil;
+        _isFire = NO;
+        _tomatoTime = 25;
+        _tomatoSeconds = _tomatoTime * 60;
+        [_player stop];
+    } else {
+        _tomatoSeconds--;
+        _timeShowLbl.text = [self getTimeSecondsStr];
+    }
+}
+
+- (NSString *)getTimeSecondsStr {
+    NSString *timeStr = @"";
+    if (_tomatoSeconds%60 < 10) {
+        if (_tomatoSeconds/60 < 10) {
+            timeStr = [NSString stringWithFormat:@"0%ld:0%ld",_tomatoSeconds/60,_tomatoSeconds%60];
+        } else {
+            timeStr = [NSString stringWithFormat:@"%ld:0%ld",_tomatoSeconds/60,_tomatoSeconds%60];
+        }
+    } else {
+        if (_tomatoSeconds/60 < 10) {
+            timeStr = [NSString stringWithFormat:@"0%ld:%ld",_tomatoSeconds/60,_tomatoSeconds%60];
+        } else {
+            timeStr = [NSString stringWithFormat:@"%ld:%ld",_tomatoSeconds/60,_tomatoSeconds%60];
         }
     }
-    _timeShowLbl.text = [NSString stringWithFormat:@"%ld分钟",_tomatoTime];
+    return timeStr;
 }
 
 #pragma mark - Init Method
@@ -55,7 +120,7 @@
     self = [super init];
     if (self) {
         self.backgroundColor = [UIColor whiteColor];
-        _tomatoTime = 25;
+        _tomatoTime = kInitialTime;
         [self addSubviewsInView];
     }
     return self;
@@ -71,6 +136,19 @@
     [self addSubview:self.timeActionBtn];
     
     [self layoutSubviewsInView];
+}
+
+- (AVAudioPlayer *)player {
+    if (!_player) {
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+        NSString *path = [[NSBundle mainBundle]pathForResource:@"薛之谦 - 演员" ofType:@"mp3"];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        _player = [[AVAudioPlayer alloc]initWithContentsOfURL:url error:nil];
+        _player.numberOfLoops = -1;
+        _player.volume = 0.5f;
+        [_player prepareToPlay];
+    }
+    return _player;
 }
 
 - (void)layoutSubviewsInView {
@@ -94,10 +172,33 @@
     
     [_segment mas_makeConstraints:^(MASConstraintMaker *make) {
         make.width.mas_equalTo(kCX_Scale(90));
-        make.height.mas_equalTo(kCX_Scale(25));
+        make.height.mas_equalTo(kCX_Scale(27));
         make.centerY.equalTo(_timeTitleLbl.mas_bottom).offset(-kCX_Scale(3));
         make.right.mas_equalTo(-kCX_Scale(15));
     }];
+    
+    [_timeActionBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.mas_equalTo(-kCX_Scale(20));
+        make.centerX.equalTo(self).multipliedBy(1.33);
+        make.width.height.mas_equalTo(kCX_Scale(88));
+    }];
+}
+
+- (NSTimer *)timer {
+    if (!_timer) {
+        _timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(timerFired:)userInfo:nil repeats:YES];
+        [[NSRunLoop currentRunLoop] addTimer:_timer forMode:NSDefaultRunLoopMode];
+    }
+    return _timer;
+}
+
+- (UIButton *)timeActionBtn {
+    if (!_timeActionBtn) {
+        _timeActionBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_timeActionBtn setImage:kIMAGE(@"play_handwrite") forState:UIControlStateNormal];
+        [_timeActionBtn addTarget:self action:@selector(timeActionBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _timeActionBtn;
 }
 
 - (UISegmentedControl *)segment {
@@ -141,22 +242,24 @@
         _leftInfoLbl.layer.borderColor = [[UIColor blackColor] CGColor];
         _leftInfoLbl.layer.borderWidth = 1;
         _leftInfoLbl.textAlignment = NSTextAlignmentCenter;
-
-        NSString *titleStr = @"TODAY\n0 mins\nPOMOS\nx 0\nDAY\nNo.1";
-        NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:titleStr];
-        NSMutableParagraphStyle *style1 = [[NSMutableParagraphStyle alloc] init];
-        style1.alignment = NSTextAlignmentCenter;
-        style1.lineSpacing = 7;
-        [attStr addAttribute:NSParagraphStyleAttributeName value:style1 range:NSMakeRange([@"TODAY\n5 mins\nPOMOS\n" length], [@"x 1\n" length])];
-        NSMutableParagraphStyle *style2 = [[NSMutableParagraphStyle alloc] init];
-        style2.alignment = NSTextAlignmentCenter;
-        style2.lineSpacing = 7;
-        [attStr addAttribute:NSParagraphStyleAttributeName value:style2 range:NSMakeRange([@"TODAY\n" length], [@"5 mins\n" length])];
         
-        _leftInfoLbl.attributedText = attStr;
-        
+        _leftInfoLbl.attributedText = [self getTitleStr:@"TODAY\n0 mins\nPOMOS\nx 0\nDAY\nNo.1"];
     }
     return _leftInfoLbl;
+}
+
+#pragma mark - 获取左边信息栏字符串
+- (NSMutableAttributedString *)getTitleStr:(NSString *)str {
+    NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:str];
+    NSMutableParagraphStyle *style1 = [[NSMutableParagraphStyle alloc] init];
+    style1.alignment = NSTextAlignmentCenter;
+    style1.lineSpacing = 7;
+    [attStr addAttribute:NSParagraphStyleAttributeName value:style1 range:NSMakeRange([@"TODAY\n5 mins\nPOMOS\n" length], [@"x 1\n" length])];
+    NSMutableParagraphStyle *style2 = [[NSMutableParagraphStyle alloc] init];
+    style2.alignment = NSTextAlignmentCenter;
+    style2.lineSpacing = 7;
+    [attStr addAttribute:NSParagraphStyleAttributeName value:style2 range:NSMakeRange([@"TODAY\n" length], [@"5 mins\n" length])];
+    return attStr;
 }
 
 
