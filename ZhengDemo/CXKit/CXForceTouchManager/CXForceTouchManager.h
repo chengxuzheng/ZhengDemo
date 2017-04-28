7a2964266c2193ff1a7d47f05798c90fc3c5ff8f
chengxuzheng <zheng890715@gmail.com>
2017-04-28 18:29:53 +0800

    go

--- ZhengDemo/CXKit/CXForceTouchManager/CXForceTouchManager.h
+++ ZhengDemo/CXKit/CXForceTouchManager/CXForceTouchManager.h
@@ -10,19 +10,21 @@
 
 @interface CXForceTouchManager : NSObject
 
-+ (instancetype _Nonnull )defaultManager;
-
 /** 检测ForceTouch是否可用 **/
 + (BOOL)checkForceTouchCapabilityAvailable;
+
 
 /** 开启设置方法 **/
 + (void)makeShortcutItems:(void(^_Nonnull)(CXForceTouchManager *_Nonnull manager))block;
 
+
 /** 添加item方法 **/
 - (CXForceTouchManager *_Nonnull(^_Nonnull)(NSString * _Nonnull type, NSString * _Nonnull title, NSString * _Nullable subtitle, NSString * _Nonnull imageName))addItem;
 
+
 /** 完成添加item方法 **/
 - (CXForceTouchManager * _Nonnull (^_Nonnull)())finished;
+
 
 /** 点击item方法 **/
 + (void)didSelectItemWithType:(NSString *_Nonnull)type;
